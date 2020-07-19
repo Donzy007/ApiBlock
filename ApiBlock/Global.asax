ba0da6c27cfaa3678058e8a7569de2577a021159
Donzy007 <donstephco@yahoo.com>
2020-07-19 18:00:08 +0100

    Add project files.

---
+++ ApiBlock/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ApiBlock.MvcApplication" Language="C#" %>
---
+++ ApiBlock/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ApiBlock.Startup))]
+namespace ApiBlock
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
